b5f713b63d3ad000b9ff428c34adf856789a58f2
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-06-18 20:33:11 -0300

    feat: adjust appointment-resolverhave a mutation to create and a query to get apointmments

--- src/resolvers/appointments-resolver.ts
+++ src/resolvers/appointments-resolver.ts
@@ -1,11 +1,23 @@
 // Como se fosse o controler do REST
 
-import { Query, Resolver } from 'type-graphql';
+import { Arg, Mutation, Query, Resolver } from 'type-graphql';
+import { CreateAppointmentInput } from '../dtos/inputs/create-appointment';
+import { Appointment } from '../dtos/models/appointment-model';
 
 @Resolver()
 export class AppointmentsResolver {
-  @Query(() => String)
-  async helloWorld() {
-    return 'Hello World';
+  @Query(() => [Appointment])
+  async appointments() {
+    return [];
+  }
+
+  @Mutation(() => Appointment)
+  async createAppointment(@Arg('data') data: CreateAppointmentInput) {
+    const appointment = {
+      startsAt: data.startsAt,
+      endsAt: data.endsAt,
+    };
+
+    return appointment;
   }
 }
